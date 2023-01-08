25fec1006ef3e0c6e5731692200ad4e8647a8187
Anna Neufeld <annaneufeld@Annas-MacBook-Pro-49.local>
2023-01-07 17:21:18 -0800

    nuissance_params_done.R

--- Figure1/nuissance_params_sims.R
+++ Figure1/nuissance_params_sims.R
@@ -1,42 +1,34 @@
-nbSample <- function(x, bprime, eps) {
-  p <- rbeta(length(x),eps*bprime,(1-eps)*bprime)
-  return(rbinom(length(x),x,p))
-}
+#### Datathin from a N(mu, sigma) distribution, assuming that sigma=sigmaprime. 
 normalSample <- function(x, sigmaprime, eps) {
   return(rnorm(length(x), eps*x, sqrt(eps*(1-eps)*sigmaprime)))
 }
 
+
+### Datathin from a NB(r,p) distribution, assuming that r=rprime. 
+nbSample <- function(x, rprime, eps) {
+  p <- rbeta(length(x),eps*rprime,(1-eps)*rprime)
+  return(rbinom(length(x),x,p))
+}
+
+#### Datathin from a Gamma(alpha, beta) distribution, assuming that alpha=alphaprime. 
 gammaSample <- function(x, alphaprime, eps) {
   Z <- rbeta(length(x), eps*alphaprime, (1-eps)*alphaprime)
   return(Z*x)
 }
 
 
-#### I was just verifying that the sampling functions are correct here. 
+### Computes the correlation between X1 and X2 when we datathin the N(mu, sigma) distribution,
+#### assuming that sigma=sigmaprime. Suggested by Prop 1. 
+exactCorExpression_normal <- function(ep,sigma, sigma.tilde) {
+  varXtrain <- ep^2*sigma+ep*(1-ep)*sigma.tilde
+  varXtest <- (1-ep)^2*sigma+ep*(1-ep)*sigma.tilde
+  covar2 <- ep*(1-ep)*(sigma-sigma.tilde)
+  cor2 <- covar2 / sqrt(varXtrain*varXtest)
+  return(cor2)
+}
 
-# n <- 100000
-# normalmu <- 5
-# normalsig <- 2
-# X <- rnorm(n, normalmu, sqrt(normalsig))
-# Xtrain <- normalSample(X, 3, 0.7)
-# Xtest <- X-Xtrain
-# cov(Xtrain, Xtest)
-# cor(Xtrain, Xtest)
-# 
-# nbR <- 5
-# nbP <- 0.7
-# X <- rnbinom(n, nbR, nbP)
-# Xtrain <- nbSample(X, 25, 0.3)
-# Xtest <- X-Xtrain
-# cor(Xtrain, Xtest)
-# 
-# alpha <- 5
-# beta <- 7
-# X <- rgamma(n, alpha, beta)
-# Xtrain <- gammaSample(X, alpha, 0.3)
-# Xtest <- X-Xtrain
-# cor(Xtrain, Xtest)
-
+### Computes the correlation between X1 and X2 when we datathin the NB(b,p) distribution
+### assuming that b=bprime. Suggested by Prop 2. 
 exactCorExpression_nb <- function(ep,p, b, bprime) {
   varXtrain <- ep*(1-ep)*b*(1-p)/((bprime+1)*p)*(bprime+1/p+b*(1-p)/p)+ep^2*b*(1-p)/p^2
   varXtest <- ep*(1-ep)*b*(1-p)/((bprime+1)*p)*(bprime+1/p+b*(1-p)/p)+(1-ep)^2*b*(1-p)/p^2
@@ -46,27 +38,39 @@ exactCorExpression_nb <- function(ep,p, b, bprime) {
   return(myCov/sqrt(varXtrain*varXtest))
 }
 
-exactCorExpression_normal <- function(ep,sigma, sigma.tilde) {
-  varX <- sigma
-  varXtrain <- ep^2*sigma+ep*(1-ep)*sigma.tilde
-  varXtest <- (1-ep)^2*sigma+ep*(1-ep)*sigma.tilde
-  #covar1 <- (varX - varXtrain - varXtest)/2
-  covar2 <- ep*(1-ep)*(sigma-sigma.tilde)
-  #cor1 <- covar1 / sqrt(varXtrain*varXtest)
-  cor2 <- covar2 / sqrt(varXtrain*varXtest)
-  return(cor2)
-}
 
+
+### Computes the correlation between X1 and X2 when we datathin the Gamma(alpha, beta) distribution,
+#### assuming that alpha=alphaprime. Suggested by Prop 3. 
 exactCorExpression_gamma <- function(ep, alpha, beta, alpha.tilde) {
   myVarXtrain <- ep*(1-ep)/(alpha.tilde+1)*(alpha/beta^2+alpha^2/beta^2)+ep^2*alpha/beta^2
   myVarXtest <- ep*(1-ep)/(alpha.tilde+1)*(alpha/beta^2+alpha^2/beta^2)+(1-ep)^2*alpha/beta^2
   myCov <- ep*(1-ep)*alpha/beta^2*(1-(alpha+1)/(alpha.tilde+1))
-
   return(myCov / sqrt(myVarXtrain*myVarXtest))
 }
 
+##### Normal Simulation. Creates the left panel of Figure 1. 
+mu <- 7
+sigma <- 5
+tildeSigmas <- 10^(seq(-log10(10), log10(1000), length.out=50))
+corsNormal<- rep(NA, length(tildeSigmas))
+varXtrainNormal <- rep(NA, length(tildeSigmas))
+varXtestNormal <- rep(NA, length(tildeSigmas))
+c <- 1
+X <- rnorm(n, mu, sqrt(sigma))
 
-##### Negative Binomial Simulation 
+for (tildeSigma in tildeSigmas) {
+  Xtrain <- sapply(X,function(u) normalSample(u, tildeSigma, ep))
+  Xtest <- X-Xtrain
+  corsNormal[c] <-cor(Xtrain, Xtest)
+  varXtrainNormal[c] <- var(Xtrain)
+  varXtestNormal[c] <- var(Xtest)
+  c <- c+1
+}
+trueCorsNormal <- sapply(tildeSigmas, function(u) exactCorExpression_normal(ep, sigma, u))
+
+
+##### Negative Binomial Simulation. Creates the center panel of Figure 1. 
 n <- 100000
 p <- 0.7
 r <- 7
@@ -89,27 +93,7 @@ for (tildeR in tildeRs) {
 trueCorsNB <- sapply(tildeRs, function(u) exactCorExpression_nb(ep,p,r,u))
 
 
-##### Normal Simulation
-mu <- 7
-sigma <- 5
-tildeSigmas <- 10^(seq(-log10(10), log10(1000), length.out=50))
-corsNormal<- rep(NA, length(tildeSigmas))
-varXtrainNormal <- rep(NA, length(tildeSigmas))
-varXtestNormal <- rep(NA, length(tildeSigmas))
-c <- 1
-X <- rnorm(n, mu, sqrt(sigma))
-
-for (tildeSigma in tildeSigmas) {
-  Xtrain <- sapply(X,function(u) normalSample(u, tildeSigma, ep))
-  Xtest <- X-Xtrain
-  corsNormal[c] <-cor(Xtrain, Xtest)
-  varXtrainNormal[c] <- var(Xtrain)
-  varXtestNormal[c] <- var(Xtest)
-  c <- c+1
-}
-trueCorsNormal <- sapply(tildeSigmas, function(u) exactCorExpression_normal(ep, sigma, u))
-
-##### Gamma Simulation
+##### Gamma Simulation. Creates the right panel of Figure 1. 
 alpha <- 7
 beta <- 5
 tildeAlphas <- 10^(seq(-log10(100), log10(1000), length.out=50))
@@ -129,6 +113,9 @@ for (tildeAlpha in tildeAlphas) {
 }
 trueCorsGamma <- sapply(tildeAlphas, function(u) exactCorExpression_gamma(ep, alpha, beta, u))
 
+
+
+#### Final plotting code.
 library(patchwork)
 library(tidyverse)
 pNB <- ggplot(data=NULL, aes(x=tildeRs, y=corsNB, col="Empirical"))+
